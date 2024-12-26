9c814fb53dc2645dcbbd9b8cbd39b50a6bb19a41
pooria <abagheri1994@gmail.com>
2024-12-26 03:37:58 +0100

    feat: add basic privacy policy

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,9 @@
+import PrivacyPolicy from './privacy-policy.mdx';
+
+export default function PrivacyPolicyPage() {
+  return (
+    <div className="container prose dark:prose-invert mx-auto my-16">
+      <PrivacyPolicy />
+    </div>
+  );
+}
