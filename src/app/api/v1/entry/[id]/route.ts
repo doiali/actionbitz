3a564bb7624c62ee0545638131f9f457d49b4584
pooria <abagheri1994@gmail.com>
2025-01-05 16:11:57 +0100

    refactor: improve route handler types

--- src/app/api/v1/entry/[id]/route.ts
+++ src/app/api/v1/entry/[id]/route.ts
@@ -1,44 +1,44 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { prisma } from '@/lib/prisma';
-import { auth } from '@/auth';
-import { NextResponse } from 'next/server';
-import { Entry } from '@prisma/client';
+import { prisma } from '@/lib/prisma'
+import { auth } from '@/auth'
+import { NextResponse } from 'next/server'
+import { EntryData, EntryJson } from '@/entities/entry'
 
 export const GET = auth(async function GET(
   req,
   { params }
 ) {
-  const userId = req?.auth?.user?.id;
-  const { id } = await params as { id: string; };
+  const userId = req?.auth?.user?.id
+  const { id } = await params as { id: string }
   if (!userId)
-    return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
+    return NextResponse.json({ message: "Not authenticated" }, { status: 401 })
 
   const entry = await prisma.entry.findFirst({
     where: { userId, id: Number(id) },
-  });
+  })
 
   if (!entry)
-    return NextResponse.json({ message: "Not Found" }, { status: 404 });
+    return NextResponse.json({ message: "Not Found" }, { status: 404 })
 
-  return NextResponse.json(({ ...entry, id: Number(entry?.id) }), { status: 200 });
-}) as any;
+  return NextResponse.json(({ ...entry, id: Number(entry?.id) } satisfies EntryData), { status: 200 })
+}) as any
 
 export const PUT = auth(async function PUT(
   req,
   { params }
 ) {
-  const userId = req?.auth?.user?.id;
-  const { id } = await (params as unknown) as { id: string; };
+  const userId = req?.auth?.user?.id
+  const { id } = await (params as unknown) as { id: string }
   if (!userId)
-    return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
-  const { title, description, datetime, type, completed } = await req.json() as Entry;
+    return NextResponse.json({ message: "Not authenticated" }, { status: 401 })
+  const { title, description, datetime, type, completed } = await req.json() as EntryJson
 
   const entry = await prisma.entry.findFirst({
     where: { userId, id: Number(id) },
-  });
+  })
 
   if (!entry)
-    return NextResponse.json({ message: "Not Found" }, { status: 404 });
+    return NextResponse.json({ message: "Not Found" }, { status: 404 })
 
   const newEntry = await prisma.entry.update({
     where: { id: Number(id), userId },
@@ -49,28 +49,28 @@ export const PUT = auth(async function PUT(
       datetime,
       completed,
     }
-  });
+  })
 
-  return NextResponse.json(({ ...newEntry, id: Number(newEntry?.id) }), { status: 200 });
-}) as any;
+  return NextResponse.json(({ ...newEntry, id: Number(newEntry?.id) } satisfies EntryData), { status: 200 })
+}) as any
 
 export const DELETE = auth(async function DELETE(req, { params }) {
-  const userId = req?.auth?.user?.id;
-  const { id } = await (params as unknown) as { id: string; };
+  const userId = req?.auth?.user?.id
+  const { id } = await (params as unknown) as { id: string }
   if (!userId)
-    return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
+    return NextResponse.json({ message: "Not authenticated" }, { status: 401 })
 
   const entry = await prisma.entry.findFirst({
     where: { userId, id: Number(id) },
-  });
+  })
 
   if (!entry)
-    return NextResponse.json({ message: "Not Found" }, { status: 404 });
+    return NextResponse.json({ message: "Not Found" }, { status: 404 })
 
   await prisma.entry.delete({
     where: { id: Number(id), userId },
-  });
+  })
 
-  return NextResponse.json(null, { status: 200 });
+  return NextResponse.json(null, { status: 200 })
 }) as any;
 
--- src/app/api/v1/entry/route.ts
+++ src/app/api/v1/entry/route.ts
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { prisma } from '@/lib/prisma';
-import { auth } from '@/auth';
-import { NextResponse } from 'next/server';
-import { Entry } from '@prisma/client';
+import { prisma } from '@/lib/prisma'
+import { auth } from '@/auth'
+import { NextResponse } from 'next/server'
+import { EntryCreate, EntryData } from '@/entities/entry'
 
 export const GET = auth(async function GET(req) {
-  const auth = req?.auth;
+  const auth = req?.auth
   if (!auth)
-    return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
-  const userId = auth.user?.id;
-  const entries = (await prisma.entry.findMany({
+    return NextResponse.json({ message: "Not authenticated" }, { status: 401 })
+  const userId = auth.user?.id
+  const entries: EntryData[] = (await prisma.entry.findMany({
     where: { userId },
     select: {
       id: true,
@@ -22,25 +22,25 @@ export const GET = auth(async function GET(req) {
     },
     orderBy: { datetime: 'desc' },
   }))
-    .map((entry) => ({ ...entry, id: Number(entry.id) }));
+    .map((entry) => ({ ...entry, id: Number(entry.id) }))
 
 
-  return NextResponse.json(({ data: entries }), { status: 200 });
-}) as any;
+  return NextResponse.json(({ data: entries }), { status: 200 })
+}) as any
 
 export const POST = auth(async function POST(req) {
-  const userId = req?.auth?.user?.id;
+  const userId = req?.auth?.user?.id
   if (!userId)
-    return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
-  const { title, description, datetime, type } = await req.json() as Entry;
+    return NextResponse.json({ message: "Not authenticated" }, { status: 401 })
+  const { title, description, datetime, type } = await req.json() as EntryCreate
   const entry = await prisma.entry.create({
     data: {
       title,
-      type,
-      userId,
       description,
+      type,
       datetime,
+      userId,
     }
-  });
-  return NextResponse.json(({ ...entry, id: Number(entry.id) }), { status: 200 });
-}) as any;
+  })
+  return NextResponse.json(({ ...entry, id: Number(entry.id) } satisfies EntryData), { status: 200 })
+}) as any
