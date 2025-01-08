071502218c0a47594cb5bb6c6be866750eb282bf
pooria <abagheri1994@gmail.com>
2025-01-08 05:11:08 +0100

    feat: complete dashboard layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,24 @@
 'use client'
 
+import { AppSidebar } from '@/components/app-sidebar'
 import ProtectedPage from '@/components/ProtectedPage'
+import ThemeToggle from '@/components/ThemeToggle'
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <ProtectedPage>
-      {/* <div className="mx-auto max-w-3xl my-8 px-4">
-        <LayoutHeader />
-      </div> */}
-      {children}
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="flex justify-between h-12 shrink-0 items-center gap-2 border-b px-4 relative">
+            <SidebarTrigger className=" -ms-2" />
+            <h1 className="grow text-center text-2xl">Action<span className="text-primary">Bitz</span></h1>
+            <ThemeToggle className=" -me-2" />
+          </header>
+          {children}
+        </SidebarInset>
+      </SidebarProvider>
     </ProtectedPage>
   )
 }
