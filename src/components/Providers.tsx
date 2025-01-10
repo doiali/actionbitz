05aee55f92128e1de0c2e0e635702e37e001a655
pooria <abagheri1994@gmail.com>
2025-01-10 22:44:28 +0100

    feat (QueryProvider): set refetch on focus for dev

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,9 +5,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false,
+      refetchOnWindowFocus: process.env.NODE_ENV === 'development' ? true : false,
       retry: 0,
-      staleTime: 300000
+      staleTime: process.env.NODE_ENV === 'development' ? 0 : 300000
     },
   },
 })
