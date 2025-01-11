6623f803f9e63c2c4383b2633b2ec4f2e5c20799
pooria <abagheri1994@gmail.com>
2025-01-11 06:11:31 +0100

    fix (app): italic brand in dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -14,7 +14,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <SidebarInset>
           <header className="bg-background sticky z-10 top-0 flex justify-between h-12 shrink-0 items-center gap-2 px-4">
             <SidebarTrigger className=" -ms-2" />
-            <h1 className="grow text-center text-2xl"><AppBrand /></h1>
+            <h1 className="grow text-center text-2xl italic"><AppBrand /></h1>
             <ThemeToggle className=" -me-2" />
           </header>
           {children}
