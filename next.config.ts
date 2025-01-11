eb0c1a575f43f4fa5090970fe3d469a01b2814f9
pooria <abagheri1994@gmail.com>
2025-01-11 21:16:46 +0100

    feat (app): add manifest and icons

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,35 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 import createMDX from '@next/mdx'
 
 const nextConfig: NextConfig = {
   /* config options here */
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+        ],
+      },
+    ]
+  },
 
-};
+}
 
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
 })
 
-export default withMDX(nextConfig);
+export default withMDX(nextConfig)
