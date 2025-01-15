cbf5eb1c7c1f4fe44496063fdcb315592f609bdb
pooria <abagheri1994@gmail.com>
2025-01-15 08:19:59 +0100

    refactor (button): remove forward ref

--- src/components/start-button.tsx
+++ src/components/start-button.tsx
@@ -3,22 +3,23 @@ import { useSession } from "next-auth/react"
 import { Button } from './ui/button'
 import Link from 'next/link'
 
-export default function StartButton() {
+const StartButton: React.FC<React.ComponentProps<typeof Button>> = (props) => {
   const session = useSession()
 
   const { status } = session
   return (
-    <div className="flex items-center justify-center">
+    <>
       {(status === 'authenticated' || status === 'loading') && (
-        <Button asChild>
+        <Button asChild {...props}>
           <Link href="/dashboard">Get started</Link>
         </Button>
       )}
       {status === 'unauthenticated' && (
-        <Button asChild>
+        <Button asChild {...props}>
           <Link href="/auth/signin">Get started</Link>
         </Button>
       )}
-    </div>
+    </>
   )
 }
+export default StartButton
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -35,13 +35,13 @@ const buttonVariants = cva(
 )
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  extends React.ComponentProps<'button'>,
+  VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+const Button: React.FC<ButtonProps> =
+  ({ className, variant, size, asChild = false, ref, ...props }) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
@@ -51,7 +51,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       />
     )
   }
-)
-Button.displayName = "Button"
+
 
 export { Button, buttonVariants }
