8a64a33e00deb1759d7d10d0b9d7fd8960b01829
pooria <abagheri1994@gmail.com>
2025-01-24 04:46:49 +0100

    feat: add copy entry functionality to entry menu

--- src/components/entry-item.tsx
+++ src/components/entry-item.tsx
@@ -1,12 +1,14 @@
 'use client'
 
-import { EntryCreate, EntryData, useEntryDelete, useEntryUpdate } from '@/entities/entry'
+import { EntryCreate, EntryData, useEntryDelete, useEntryUpdate, useEntryCreate } from '@/entities/entry'
 import EntryForm from './entry-form'
 import { useState } from 'react'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import { Button } from '@/components/ui/button'
 import { EllipsisVerticalIcon } from '@heroicons/react/24/outline'
 import { CheckboxTodo } from '@/components/ui/checkbox-todo'
+import { Dialog, DialogContent, DialogDescription, DialogTitle } from '@/components/ui/dialog'
+import { startOfToday } from 'date-fns'
 
 const EntryItem = ({ entry }: {
   entry: EntryData
@@ -51,7 +53,9 @@ const EntryItem = ({ entry }: {
 export default EntryItem
 
 const EntryMenu = ({ entry }: { entry: EntryData }) => {
-  const mutation = useEntryDelete()
+  const [copyDialogOpen, setCopyDialogOpen] = useState(false)
+  const deleteMutation = useEntryDelete()
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -60,11 +64,70 @@ const EntryMenu = ({ entry }: { entry: EntryData }) => {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-        <DropdownMenuItem onClick={() => mutation.mutate(entry)}>
+        <DropdownMenuItem onClick={() => deleteMutation.mutate(entry)}>
           Delete
         </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setCopyDialogOpen(true)}>
+          Copy
+        </DropdownMenuItem>
       </DropdownMenuContent>
+      <Dialog open={copyDialogOpen} onOpenChange={setCopyDialogOpen}>
+        <DialogContent>
+          <DialogTitle>Copy Action</DialogTitle>
+          <DialogDescription asChild>
+            <div>
+              <EntryCopyForm entry={entry} onClose={() => setCopyDialogOpen(false)} />
+            </div>
+          </DialogDescription>
+        </DialogContent>
+      </Dialog>
     </DropdownMenu>
+  )
+}
+
+const getInitialCopyEntry: (entry: EntryData) => EntryCreate = (entry) => ({
+  title: entry.title,
+  type: 'TODO',
+  description: entry.description,
+  completed: false,
+  datetime: null,
+  date: startOfToday(),
+})
+
+const EntryCopyForm = ({ entry, onClose }: {
+  entry: EntryData
+  onClose: () => void
+}) => {
+  const [state, setState] = useState<EntryCreate>(getInitialCopyEntry(entry))
+
+  const mutation = useEntryCreate({
+    onSuccess: () => handleClose()
+  })
+
+  const onChange = <T extends keyof EntryCreate>(name: T, value: EntryCreate[T]) => {
+    setState((prev) => ({
+      ...prev,
+      [name]: value,
+    }))
+  }
+
+  const handleClose = () => {
+    onClose()
+    setState(getInitialCopyEntry(entry))
+  }
+
+  const onSubmit = () => {
+    mutation.mutate(state)
+  }
+
+  return (
+    <EntryForm
+      state={state}
+      onChange={onChange}
+      onSubmit={onSubmit}
+      onCancel={handleClose}
+      disabled={mutation.isPending}
+    />
   )
 }
 
