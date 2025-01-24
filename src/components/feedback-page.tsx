330b445f7c3f5c252827eaa725c0171be34f9510
pooria <abagheri1994@gmail.com>
2025-01-24 06:07:21 +0100

    fix (app): change support email

--- src/components/feedback-page.tsx
+++ src/components/feedback-page.tsx
@@ -5,7 +5,7 @@ export default function FeedbackPage() {
     <div className="w-full max-w-3xl flex mx-auto px-4 py-4 pb-12">
       <div className="prose dark:prose-invert">
         <h1 className="text-3xl font-bold mb-8">We Value Your Feedback!</h1>
-        <p>Email: <a href="mailto:support@actionbitz.com">support@actionbitz.com</a> </p>
+        <p>Email: <a href="mailto:doialibgr@gmail.com">doialibgr@gmail.com</a> </p>
         <p>We’re always looking to make Actionbitz better. Got an idea or suggestion? Drop us a message and let us know how we can improve your experience. Your thoughts help shape the future of the app!</p>
       </div>
     </div>
--- src/components/support-page.tsx
+++ src/components/support-page.tsx
@@ -5,7 +5,7 @@ export default function SupportPage() {
     <div className="w-full max-w-3xl flex mx-auto px-4 py-4 pb-12">
       <div className="prose dark:prose-invert">
         <h1 className="text-3xl font-bold mb-8">Support</h1>
-        <p>Email: <a href="mailto:support@actionbitz.com">support@actionbitz.com</a> </p>
+        <p>Email: <a href="mailto:doialibgr@gmail.com">doialibgr@gmail.com</a> </p>
         <p>Feel free to reachout to us. Seriously, don’t hesitate—we’re nice people. Remember: no question is too small, and no todo is too big for Actionbitz!</p>
       </div>
     </div>
